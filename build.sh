792ed0504eaa4b3ef62a44e1e80579671255a083
mpolatcan <mutlupolatcan@gmail.com>
2019-10-08 11:38:19 +0300

    alpine versions removed due to lack of glibc

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+SPARK_TYPES=(
+   "spark"
+   "pyspark"
+   "sparkr"
+)
+
 SPARK_VERSIONS=(
     "2.4.4"
     "2.3.4"
@@ -10,32 +16,28 @@ HADOOP_VERSIONS=(
     "2.6"
 )
 
-SPARK_TYPES=(
-   "spark"
-   "pyspark"
-   "sparkr"
-)
-
-DISTS=(
-    "alpine"
-    "ubuntu"
+JAVA_VERSIONS=(
+    "8"
+    "9"
+    "10"
+    "11"
 )
 
 # $1: SPARK_TYPE
-# $2: DIST
-# $3: SPARK_VERSION
-# $4: HADOOP_VERSION
+# $2: SPARK_VERSION
+# $3: HADOOP_VERSION
+# $4: JAVA_VERSION
 function build_image() {
-  sudo docker build -q -t mpolatcan/$1:$2-$3-hadoop-$4 --build-arg SPARK_VERSION=$3 --build-arg HADOOP_VERSION=$4 ./$2/$1/
-	sudo docker push mpolatcan/$1:$2-$3-hadoop-$4
-	sudo docker rmi mpolatcan/$1:$2-$3-hadoop-$4
+  sudo docker build -q -t mpolatcan/$1:$2-hadoop-$3-java$4 --build-arg SPARK_VERSION=$2 --build-arg HADOOP_VERSION=$3 --build-arg JAVA_VERSION=$4 ./src/$1/
+	sudo docker push mpolatcan/$1:$2-hadoop-$3-java$4
+	sudo docker rmi mpolatcan/$1:$2-hadoop-$3-java$4
 }
 
 for SPARK_TYPE in ${SPARK_TYPES[@]}; do
     for SPARK_VERSION in ${SPARK_VERSIONS[@]}; do
         for HADOOP_VERSION in ${HADOOP_VERSIONS[@]}; do
-            for DIST in ${DISTS[@]}; do
-                build_image $SPARK_TYPE $DIST $SPARK_VERSION $HADOOP_VERSION
+            for JAVA_VERSION in ${JAVA_VERSIONS[@]}; do
+                build_image $SPARK_TYPE $SPARK_VERSION $HADOOP_VERSION $JAVA_VERSION
             done
         done
     done
