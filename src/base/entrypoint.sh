4fde14b008e0ee43a3c410c8be3b8f2ac71d2c94
mpolatcan <mutlupolatcan@gmail.com>
2019-11-22 13:58:37 +0300

    spark deployments updated and dockerfile updated

--- src/base/entrypoint.sh
+++ src/base/entrypoint.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
+declare -A spark_services
+
+spark_services[livy]="livy-server start"
+
 ./hadoop_entrypoint.sh $1
 
 ./spark_config_loader.sh
 
-for daemon in ${HADOOP_DAEMONS[@]}; do
-    if [[ "$daemon" == "namenode" ]]; then
-      echo "Awaiting Hadoop services to be started..."
-      sleep 10
-      echo "Starting Apache Livy server..."
-      livy-server start
-    fi
-done
+if [[ ${SPARK_SERVICES} != "NULL" ]]; then
+  echo "Awaiting Hadoop services to be started..."
+  sleep 10
+  echo "Starting Spark services..."
+  for spark_service in ${SPARK_SERVICES[@]}; do
+    ${spark_services[$spark_service]}
+  done
+fi
 
 if [[ "$1" == "spark" ]]; then
   tail -f /dev/null
